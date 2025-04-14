0c972e4fe770d60e4fa3e4ab7c9b8cfb621363a1
Yuri <yuri-chrystian@outlook.com>
2025-04-13 21:54:47 -0300

    Feito a tela de SignIn

--- app/src/main/java/br/edu/unifaj/mobile/fajbank/GatewayActivity.java
+++ app/src/main/java/br/edu/unifaj/mobile/fajbank/GatewayActivity.java
@@ -1,17 +1,32 @@
 package br.edu.unifaj.mobile.fajbank;
 
+import android.content.Intent;
 import android.os.Bundle;
-import android.os.PersistableBundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
-public class GatewayActivity extends AppCompatActivity {
+import br.edu.unifaj.mobile.fajbank.feature.user.signin.SignInActivity;
 
+public class GatewayActivity extends AppCompatActivity {
+    Button login;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_gateway);
+        login = findViewById(R.id.main_button_login);
+        loginButton(login);
+    }
+
+    public void loginButton(Button login){
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(GatewayActivity.this, SignInActivity.class));
+            }
+        });
     }
 }
---
+++ app/src/main/java/br/edu/unifaj/mobile/fajbank/feature/user/signin/SignInActivity.java
@@ -0,0 +1,16 @@
+package br.edu.unifaj.mobile.fajbank.feature.user.signin;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import br.edu.unifaj.mobile.fajbank.R;
+
+public class SignInActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_singin);
+    }
+}
