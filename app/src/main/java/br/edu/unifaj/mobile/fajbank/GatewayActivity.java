c308f4324a4138a8d27ff6259dcabfdc28bb4045
YuriChrystian <yuri-chrystian@outlook.com>
2025-05-05 21:41:33 -0300

    Inicializado a contrução da tela de cadastro

--- app/src/main/java/br/edu/unifaj/mobile/fajbank/GatewayActivity.java
+++ app/src/main/java/br/edu/unifaj/mobile/fajbank/GatewayActivity.java
@@ -9,9 +9,11 @@ import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import br.edu.unifaj.mobile.fajbank.feature.user.signin.SignInActivity;
+import br.edu.unifaj.mobile.fajbank.feature.user.singup.SingUpActiivity;
 
 public class GatewayActivity extends AppCompatActivity {
     Button login;
+    Button register;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -19,6 +21,8 @@ public class GatewayActivity extends AppCompatActivity {
         setContentView(R.layout.activity_gateway);
         login = findViewById(R.id.main_button_login);
         loginButton(login);
+        register = findViewById(R.id.main_button_cadastrar);
+        registerButton(register);
     }
 
     public void loginButton(Button login){
@@ -27,7 +31,14 @@ public class GatewayActivity extends AppCompatActivity {
             public void onClick(View v) {
                 startActivity(new Intent(GatewayActivity.this, SignInActivity.class));
             }
-
+        });
+    }
+    public void registerButton(Button register){
+        register.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(GatewayActivity.this, SingUpActiivity.class));
+            }
         });
     }
 }
